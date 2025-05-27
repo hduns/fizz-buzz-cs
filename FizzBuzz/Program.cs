d650576f79b0b2b05fa498342a265eb038a465ed
Hannah Dunsmore <hannah.dunsmore@softwire.com>
2025-05-27 13:30:12 +0100

    added fezz and reverse

--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 
 static void FizzBuzz()
 {
-    for (int i = 1; i < 34; i++)
+    for (int i = 1; i < 256; i++)
     {
         string numberString = createWord(i);
         Console.WriteLine(numberString);
@@ -27,6 +27,33 @@ static string createWord(int i)
         words.Clear();
         words.Add("Bong");
     }
+
+    if (i % 13 == 0)
+    {
+        if (words.Count > 0)
+        {
+            bool addedFezz = false;
+
+            for (int k = 0; k < words.Count; k++)
+            {
+                if (addedFezz) break;
+                char firstChar = words[k][0];
+                if (firstChar == 'B')
+                {
+                    words.Insert(k, "Fezz");
+                    addedFezz = true;
+                    break;
+                }
+            }
+            if(!addedFezz) words.Add("Fezz");
+        }
+        else
+        {
+            words.Add("Fezz");
+        }
+    }
+
+    if (i % 17 == 0) words.Reverse();
 
     return words.Count > 0 ? String.Join("", words) : i.ToString();
     
